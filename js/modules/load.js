994dd82c89f701ffa944af161228e33b247664d6
Miguel <luismigueldasilva.99@gmail.com>
2022-04-28 16:29:29 -0300

    load2

--- js/modules/load.js
+++ js/modules/load.js
@@ -21,7 +21,7 @@ export function load(){
     
             } finally {
     
-                document.querySelector(".load").style = "display: none;"
+                document.querySelector("[load]").style = "display: none;"
                 document.querySelector(".wrapper").style = ""
             }
         
