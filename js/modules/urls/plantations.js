dacbe81af7dfae996932ae605f46b910b5990d73
Luis Miguel Corrêa <89203851+correalm@users.noreply.github.com>
2022-05-02 14:11:37 -0300

    Update plantations.js

--- js/modules/urls/plantations.js
+++ js/modules/urls/plantations.js
@@ -1,1 +1,1 @@
-export const plantations = "https://justcors.com/tl_8592c14/https://farmbox.cc/api/public/technical_visit_report/plantations.json?token=379238b5-705c-48bc-b8c9-27e26676b417"
+export const plantations = "https://justcors.com/tl_f569cd7/https://farmbox.cc/api/public/technical_visit_report/plantations.json?token=379238b5-705c-48bc-b8c9-27e26676b417"
