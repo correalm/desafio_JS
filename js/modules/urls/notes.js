ed665ec6fcac8e26512198ca6f9dbd623c3da913
Luis Miguel Corrêa <89203851+correalm@users.noreply.github.com>
2022-05-02 14:11:15 -0300

    Update notes.js

--- js/modules/urls/notes.js
+++ js/modules/urls/notes.js
@@ -1,1 +1,1 @@
-export const notes = "https://justcors.com/tl_8592c14/https://farmbox.cc/api/public/technical_visit_report/notes.json?token=379238b5-705c-48bc-b8c9-27e26676b417"
+export const notes = "https://justcors.com/tl_f569cd7/https://farmbox.cc/api/public/technical_visit_report/notes.json?token=379238b5-705c-48bc-b8c9-27e26676b417"
