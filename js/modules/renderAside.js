9457bfba2b231b3644d6721bae54eeec1c773d4c
Luis Miguel Corrêa <89203851+correalm@users.noreply.github.com>
2022-05-02 14:37:37 -0300

    Update renderAside.js

--- js/modules/renderAside.js
+++ js/modules/renderAside.js
@@ -7,83 +7,11 @@ export async function renderAside(){
         return res
     })
 
-    // const plantations = await getPlantations().then(res => {
-    //     console.log(res)
-    //     return res  
-    // })
-    const farm = {
-        results : [
-            {
-                area: 30.00,
-                date: "2025-11-01",
-                emergence_date: "2025-11-21",
-                emergence_prediction_date: "2026-11-21",
-                harvest_prediction_date: "2026-12-12",
-                cycle: 1,
-                harvest: {
-                    name: "2025/26"
-                },
-                id: 66666,
-                name: "Testando Obj",
-                state: "active",
-                variety: {
-                    name: "coloridão"
-                }
-            },
-            {
-                area: 60.00,
-                date: "1999-07-26",
-                emergence_date: "2025-11-17",
-                emergence_prediction_date: "2023-08-09",
-                harvest_prediction_date: "1984-01-01",
-                cycle: 1,
-                harvest: {
-                    name: "1984/99"
-                },
-                id: 66667,
-                name: "Absurdo",
-                state: "active",
-                variety: {
-                    name: "Amarelão"
-                }
-            },
-            {
-                area: 90.00,
-                date: "1889-07-26",
-                emergence_date: "2155-11-17",
-                emergence_prediction_date: "2333-08-09",
-                harvest_prediction_date: "1900-01-01",
-                cycle: 1,
-                harvest: {
-                    name: "2000/01"
-                },
-                id: 66668,
-                name: "Absurdamente certo",
-                state: "active",
-                variety: {
-                    name: "Verdão"
-                }
-            },
-            {
-                area: 120.00,
-                date: "1988-01-01",
-                emergence_date: "1988-02-02",
-                emergence_prediction_date: "1988-03-03",
-                harvest_prediction_date: "1988-04-04",
-                cycle: 1,
-                harvest: {
-                    name: "1988/99"
-                },
-                id: 66669,
-                name: "Absurdamente certo novamente",
-                state: "active",
-                variety: {
-                    name: "Constituição"
-                }
-            }
+    const plantations = await getPlantations().then(res => {
+         console.log(res)
+         return res  
+    })
     
-        ]
-    }
 
     const plantations= farm
 
